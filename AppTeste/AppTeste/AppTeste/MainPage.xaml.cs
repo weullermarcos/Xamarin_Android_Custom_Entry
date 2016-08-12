df8e89ee08b139b064d24906d9da5f455e303000
wmsantos1 <wmsantos1@STFBSBC032596-L>
2016-08-12 16:53:43 -0300

    Adicionando Projeto

---
+++ AppTeste/AppTeste/AppTeste/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTeste
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+                CompanyTypePicker.Items.Add(string.Format("Empresa {0}", i));
+                FirstActivityTypePicker.Items.Add(string.Format("Atividade {0}", i));
+                SecondActivityTypePicker.Items.Add(string.Format("Atividade {0}", i));
+            }
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+        }
+    }
+}
